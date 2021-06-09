bc0e94df90c91774afbfe421bae294457aebaf6a
Athanasios Chalvatzaras <chalvatzaras@gmail.com>
2021-06-09 17:44:54 +0300

    synchronized keyword

---
+++ src/sync2/App.java
@@ -0,0 +1,74 @@
+package sync2;
+
+import java.util.Scanner;
+
+class Processor extends Thread{
+
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while(running){
+            System.out.println("Hi");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown(){
+        running = false;
+    }
+}
+
+public class App {
+
+    private int count = 0;
+
+    public synchronized void increment(){
+        count++;
+    }
+
+    public static void main(String[] args){
+        App app = new App();
+        app.doWork();
+
+    }
+
+    public void doWork(){
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10000; i++){
+                    increment();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10000; i++){
+                    increment();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ;
+
+        System.out.println("Count is: " + count);
+
+    }
+}
